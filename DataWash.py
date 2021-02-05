ed7d7f7a103f487ca2aa3972152cab7d28bc91ea
lyncer <674330831@qq.com>
2021-02-05 13:53:25 +0800

    修复了在表格中使用中文输入法直接键入 程序崩溃的问题

--- DataWash.py
+++ DataWash.py
@@ -17,7 +17,7 @@ tomorrow = tomorrow.strftime('%Y.%m.%d')
 #=======================================================================================================================
 
 
-
+#===========生成分析表格============
 def table_analyse(table,table_time):
     df = Logic.Table.table_to_df(table,table_time)
     time_col_list = ['调妥时间', '封堵开始', '封堵结束', '装车开始', '装车完毕', '平车开始', '平车结束', '具备挂车条件', '挂车时间']
@@ -29,7 +29,7 @@ def table_analyse(table,table_time):
         for i, v in df[time_col_name].items():
             try:
                 time_time = parse(v)
-                # time_str = datetime.strftime(time_time,'%Y-%m-%d %H:%M')
+                # time_str = datetime.strftime(time_time,'%H:%M')
                 df.loc[i, time_col_name] = time_time
             except:
                 pass
@@ -54,11 +54,40 @@ def table_analyse(table,table_time):
         try:
             time = (df1.loc[i, '挂车时间'] - df1.loc[i, '具备挂车条件'])/timedelta(minutes=60)
             df1.loc[i, '待挂用时'] = time
-            print(time,'\n',i)
             df1['待挂用时'] = df1['待挂用时'].apply(series_round)
             df1['待挂用时'] = df1['待挂用时'].apply(time_lessthan_zero)
         except TypeError:
             df1.loc[i, '待挂用时'] = 0
+#==================调整各个时间列的格式从 ‘xxxx-xx-xx xx:xx’ 至 ‘xx：xx’
+    for time_col_name in time_col_list:
+        df1[time_col_name] = df1[time_col_name].fillna('')
+        df1[time_col_name] = df1[time_col_name].astype('str')
+        df1[time_col_name] = '{} '.format(table_time) + df1[time_col_name]
+        df1[time_col_name] = df1[time_col_name].replace('{} '.format(table_time), '')
+        for i, v in df1[time_col_name].items():
+            try:
+                time_time = parse(v)
+                time_str = datetime.strftime(time_time,'%H:%M')
+                df1.loc[i, time_col_name] = time_str
+            except:
+                pass
+
+    #=======小时数转为 xx小时xx分钟的函数===
+    def transform_time(hours):# 例如5.5
+        # 得到总秒数
+        seconds = hours * 3600
+        m, s = divmod(seconds, 60)
+        h, m = divmod(m, 60)
+        time = "%d小时%02d分钟%02d秒" % (h, m, s)
+        return seconds
+
+        the_column_need_transform_to_hour_minute = ['线内用时','待挂用时']
+        for column_name in the_column_need_transform_to_hour_minute:
+            try:
+                df1[column_name] = 0
+            except ImportError:
+                continue
+
     df1 = df1.iloc[:,1:]
     try:
         df1.to_excel('{}分析表.xlsx'.format(table_time))
@@ -66,7 +95,6 @@ def table_analyse(table,table_time):
         remark_dialog = QDialog()
         QMessageBox.critical(remark_dialog, "注意", "请先关闭已打开的分析表", QMessageBox.Ok | QMessageBox.Cancel,
                              QMessageBox.Ok)
-
     return df1
 
 
